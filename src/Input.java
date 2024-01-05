836d89be1d7cc304f6650041c501dfc64c972c25
anand <do4connect@gmail.com>
2024-01-05 12:47:36 +0530

    Data Type

--- src/Input.java
+++ src/Input.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Input {
+/*
+Class Program For Taking Input
+**/
+import java.util.Scanner;//util means utilities
+
+class InputClass{
+    private byte a, b;
+    public void setValue(){
+        Scanner InputScanner = new Scanner(System.in);
+        a = InputScanner.nextByte();
+        b = InputScanner.nextByte();
+    }
+    public void displayValue(){
+        System.out.println("Value of a is "+ a + " Value of b is " +b);
+    }
+}
+ public class Input{
+    public static void main(String[] args){
+        InputClass obj = new InputClass();
+        obj.setValue();
+        obj.displayValue();
+    }
 }
--- src/TestRun.java
+++ src/TestRun.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class TestRun {
+public class TestRun {
+    public static void main(String[] args){
+        System.out.println("This is a test run");
+    }
 }
