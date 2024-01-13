ced74190fc6e751f19b51580c6b54084890b055b
anand <do4connect@gmail.com>
2024-01-13 08:12:54 +0530

    Class and Object

---
+++ src/InitializationBlock.java
@@ -0,0 +1,13 @@
+class Block{
+    private int a;
+
+    public void Block(){
+
+
+    }
+}
+public class InitializationBlock {
+    public static void main(String[] args){
+     Block obj = new Block();
+    }
+}
--- src/TestRun.java
+++
@@ -1,9 +0,0 @@
-public class TestRun{
-    public static void main(String[] args){
-        int add=0;
-        for (String arg : args) {
-            add = add + Integer.parseInt(arg);
-        }
-        System.out.println(add);
-    }
-}
